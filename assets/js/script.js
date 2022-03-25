5b9d5b3041fbe6a4d99ad56d071675a8a59acdc4
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-03-25 18:06:19 +0800

    Update styles and functionalities to index file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,195 @@
 // Used for index.html
-$( function() {
 
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 1895,
-      max: 2022,
-      values: [ 2012, 2022 ],
-      slide: function( event, ui ) {
-        // Date Range Input
-        $( "#date-range" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-      }
-    });
+var ratingfrom = $("#ratingfrom");
+var ratingto = $("#ratingto");
+var daterange = $("#date-range");
+var titlesearch = $("#title-search");
+var chkG = $("#chkG");
+var chkPG_13 = $("#chkPG_13");
+var chkR = $("#chkR");
+var chkNC_17 = $("#chkNC_17");
+const APIKEY = "k_y4wdrion";
+var formEl = $("#form-input");
+var loader = $("#loading");
 
-    // Date Range Static
-    $( "#date-range" ).val( $( "#slider-range" ).slider( "values", 0 ) +
-      " - " + $( "#slider-range" ).slider( "values", 1 ) );
+
+// Loader methods
+function displayLoading() {
+  loader.addClass("display");
+  setTimeout(() => {
+    loader.removeClass("display");
+  }, 10000);
+}
+function HideLoading() {
+  loader.removeClass("display");
+}
+
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  displayLoading();
+  var resultDiv = $(".table_result");
+  resultDiv.text("");
+  var parameter = [];
+  if (!isEmpty(titlesearch.val())) {
+    parameter.push("title=" + titlesearch.val());
+  }
+  if (!isEmpty(daterange.val())) {
+    parameter.push("release_date=" + daterange.val());
+  }
+
+  parameter.push("user_rating=" + ratingfrom.val() + "," + ratingto.val());
+  var certificates = [];
+  if (chkG.is(":checked")) {
+    certificates.push(chkG.val());
+  }
+  if (chkPG_13.is(":checked")) {
+    certificates.push(chkG.val());
+  }
+  if (chkR.is(":checked")) {
+    certificates.push(chkR.val());
+  }
+  if (chkNC_17.is(":checked")) {
+    certificates.push(chkNC_17.val());
+  }
+  if (certificates.length > 0) {
+    parameter.push("certificates=" + certificates.toString());
+  }
+  if (parameter.length > 0) {
+    var url = `https://imdb-api.com/API/AdvancedSearch/${APIKEY}?${parameter.join(
+      "&"
+    )}`;
+    getMovieFromURL(url);
+  } else {
+    HideLoading();
+    console.log("URL-empty");
+  }
+}
+
+//Load table helper method
+function LoadTable(data) {
+  var table = $("<table>");
+  var row = $("<tr>");
+  var col1 = $("<td>").attr("rowspan", "3");
+  var col2 = $("<td>").text(`Genres : ${data.genres} `);
+  var img = $("<img>").addClass("row_image").attr("src", data.image);
+  col1.append(img);
+  row.append(col1);
+  row.append(col2);
+  table.append(row);
+  var row1 = $("<tr>");
+
+  var col3 = $("<td>").text(`Certificate : ${data.contentRating} `);
+
+  row1.append(col3);
+  table.append(row1);
+  var row2 = $("<tr>").append($("<td>")).append($("<td>"));
+  var col4 = $("<td>").attr("rowspan", "3");
+  var alink = $("<a>")
+    .addClass("lnkclass")
+    .attr("href", `./review.html?id=${data.id}`)
+    .text("More");
+  col4.append(alink);
+  row2.append(col4);
+  table.append(row2);
+  return table;
+}
+
+//Add search result to the div
+function LoadGridMovieDetails(data) {
+  if (data.length > 0) {
+    var resultDiv = $(".table_result");
+    var maxlength = data.length > 30 ? 30 : data.length;
+
+    for (var i = 0; i < maxlength; i++) {
+      var rowdiv = $("<div>")
+        .addClass("row")
+        .attr("data-mode", "down")
+        .text(data[i].title);
+
+      var icon = $("<icon>").addClass("fa fa-angle-down");
+      var toggdiv = $("<div>").addClass("panel");
+
+      toggdiv.append(LoadTable(data[i]));
+
+      rowdiv.append(icon);
+      rowdiv.append(toggdiv);
+      rowdiv.on("click", rowclick);
+      resultDiv.append(rowdiv);
+    }
+  }
+  return;
+}
+
+//Slider method for row click
+function rowclick(event) {
+  console.log("click");
+  var rowe = $(event.target);
+  var data_mode = rowe.attr("data-mode");
+  var obbj = rowe.find(".panel");
+  var icon = rowe.find("icon");
+  if (data_mode == "down") {
+    rowe.css("background-color", "hsl(6.39, 73.16%, 54.71%)");
+    icon.removeClass("fa fa-angle-down").addClass("fa fa-angle-up");
+    rowe.attr("data-mode", "up");
+    obbj.slideDown();
+  } else {
+    rowe.css("background-color", "rgb(245, 140, 128)");
+    icon.removeClass("fa fa-angle-up").addClass("fa fa-angle-down");
+    rowe.attr("data-mode", "down");
+    obbj.slideUp("slow");
+  }
+}
+
+//Api call to get movies
+function getMovieFromURL(apiUrl) {
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        LoadGridMovieDetails(data.results);
+        HideLoading();
+      });
+    } else {
+      HideLoading();
+      document.location.replace("./index.html");
+    }
+  });
+}
+function isEmpty(value) {
+  return value == undefined || value == null || value.length === 0;
+}
+$(function () {
+  $("#slider-range").slider({
+    range: true,
+    min: 1895,
+    max: 2022,
+    values: [2012, 2022],
+    slide: function (event, ui) {
+      // Date Range Input
+      $("#date-range").val(ui.values[0] + "," + ui.values[1]);
+    },
+  });
+
+  // Date Range Static
+  $("#date-range").val(
+    $("#slider-range").slider("values", 0) +
+      "," +
+      $("#slider-range").slider("values", 1)
+  );
 });
+
+function LoadSelectInput() {
+  for (var i = 1; i <= 10; i = i + 0.1) {
+    $("<option>")
+      .val(i.toFixed(2))
+      .text(i.toFixed(2))
+      .appendTo(".select-group");
+  }
+
+  ratingto.val("10.00");
+}
+
+
+LoadSelectInput();
+formEl.on("submit", formSubmitHandler);
+
