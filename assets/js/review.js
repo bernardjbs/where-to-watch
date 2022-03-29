5da92372ae65e91a673392d81d8dcb56a41dbac4
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-03-29 21:14:17 +0800

    backing up

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,7 +1,7 @@
 const IMDB_apiKey = ["k_y4wdrion", "k_ie3pfgw9", "k_o18qbud0", "k_tfp7sn7o", "k_51hudf0k", "k_zz238cmu"];
 const TMBD_apiKey = "30584b857c462403f3b7916157ab32b7";
 let IMBD_personIDs = [];
-let API_index = 4;
+let API_index = 1;
 
 
 let actorsEl = document.querySelector("#actors");
@@ -20,9 +20,10 @@ function getParams() {
     return movieID;
 }
 
-function getApi(movieId) {
+function getMovieObject(movieId) {
     // Title API url from IMDB 
     const IMDB_requestUrl_Title = `https://imdb-api.com/en/API/Title/${IMDB_apiKey[API_index]}/${movieId}/FullCast.Posters`
+
 
     // Fetch request to Title API from IMBD
     fetch(IMDB_requestUrl_Title)
@@ -30,39 +31,55 @@ function getApi(movieId) {
             return response.json();
         })
         .then(IMDB_title_data => {
-            return getMovie(IMDB_title_data);
+            return getMovie_IMDB_TITLE(IMDB_title_data);
         })
-        .then(movie => {
+        .then(Movie => {
             // console.log("Movie in getApi 01: " + JSON.stringify(movie));
-            return getApi_TMDB(movie);
+            return getApi_TMDB(Movie);
         })
 }
 
-function getApi_TMDB(movie) {
-    // Get Person details API url from TMBD
+function getApi_TMDB(Movie) {
+
+    for(i=0; i<Movie.mainCast.length; i++) {
+        console.log("Main cast len: " + Movie.mainCast[i]);
+    }
+
+    // URL for TMDB FIND API
     const TMDB_requestUrl_Person = `https://api.themoviedb.org/3/find/nm1500155?api_key=${TMBD_apiKey}&language=en-US&external_source=imdb_id`
+
+    // Fetch request to Find API from TMDB
     fetch(TMDB_requestUrl_Person)
         .then(function (response) {
             return response.json();
         })
         .then(data => {
-            //Get the tmdb movie id and set it to the movie object
+
+            for (let key in data) {
+                console.log(key + ": ", data[key]);
+            }
+
+            //Get the tmdb person id and set it to the movie object
             let id = data.person_results[0].id;
-            movie.tmdbId = id;
-            return movie;
+            Movie.tmdbId = id;
+            return Movie;
         })
-        .then(movie => {            
-            console.log("The latest movie object: " + JSON.stringify(movie));
-        });
+        .then(Movie => {
+            console.log("The latest Movie Obj: " + JSON.stringify(Movie));
+        })
 }
 
-function getMovie(IMDB_title_data) {
-    movie = {
+function getMovie_IMDB_TITLE(IMDB_title_data) {
+    Movie = {
+        imdbMovieID: IMDB_title_data.id,
+        movieDescription: IMDB_title_data.plot,
+        movieReleaseDate: IMDB_title_data.releaseDate,
+        movieGenres: IMDB_title_data.genres,
+        movieCertificate: IMDB_title_data.contentRating,
         ...getCastNames(IMDB_title_data),
         ...getPosterLink(IMDB_title_data),
     }
-    //getApi_TMDB(movie);
-    return movie;
+    return Movie;
 }
 
 function getPosterLink(IMDB_title_data) {
@@ -74,24 +91,29 @@ function getPosterLink(IMDB_title_data) {
 }
 function getCastNames(IMDB_title_data) {
     const fullCast = getNestedObject(IMDB_title_data, ["fullCast", "actors"]);
-    let mainCast = [];
+    let mainCastArr = [];
+    let mainCastObj = {};
     const numberOfCast = 3;
 
     for (i = 0; i < numberOfCast; i++) {
-        mainCast.push(getNestedObject(fullCast, [i, "name"]));
+        mainCastObj = {
+            imdbCastID: getNestedObject(fullCast, [i, "id"]), 
+            castName: getNestedObject(fullCast, [i, "name"])
+        }
+        mainCastArr.push(mainCastObj);
     }
 
-    for (let key in IMDB_title_data) {
-        console.log(key + ": ", IMDB_title_data[key]);
-    }
+    // for (let key in IMDB_title_data) {
+    //     console.log(key + ": ", IMDB_title_data[key]);
+    // }
 
     return {
         movieTitle: IMDB_title_data.title,
-        mainCast: mainCast
+        mainCast: mainCastArr
     }
 }
 
-getApi(getParams());
+getMovieObject(getParams());
 
 //MOVIE OBJECT
 // let Movie = {
@@ -100,7 +122,7 @@ getApi(getParams());
 //     movieTitle: string,
 //     movieDescription: string,
 //     movieReleaseDate: Date,
-//     movieGendre: string,
+//     movieGenres: string,
 //     movieCertificate: string, // "PG", "G", "PG-13"
 //     movieImage: string,
 //     mainCast: [{imdbCastID: number,
@@ -113,11 +135,15 @@ getApi(getParams());
 //     }],
 //     movieImdbRating: string, ///{lang?}/API/Ratings/{apiKey}/{id} --> imDb
 //     movieAudienceReview: number, //{lang?}/API/UserRatings/{apiKey}/{id} --> .totalRatingVotes
-//     movieReviews: [{
+//     movieGoodReviews: [{
 //         reviewerName: string, //{lang?}/API/Reviews/{apiKey}/{id} --> username
-//         // if reviewScore > x, thumbs up else thumbs down??
 //         reviewerScore: number, //{lang?}/API/Reviews/{apiKey}/{id} --> rate
 //         reviewerComment: string //{lang?}/API/Reviews/{apiKey}/{id} --> title
+//     }],
+//     movieBadReviews: [{
+//         reviewerName: string,
+//         reviewerScore: number,
+//         reviewerComment: string
 //     }],
 //     streamServices: [{
 //         streamSvcID: number,
